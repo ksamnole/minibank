41d2669cb56cd61054fd8e1afb57bc07123ac316
Maxim <atm.tsvetk@gmail.com>
2022-02-25 17:11:16 +0500

    Create asp.net project

---
+++ Minibank.Core/CurrencyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minibank.Core
+{
+    public class CurrencyConverter : ICurrencyConverter
+    {
+        private readonly IExchangeRates exchangeRates;
+
+        public CurrencyConverter(IExchangeRates exchangeRates)
+        {
+            this.exchangeRates = exchangeRates;
+        }
+
+        public float Convert(float amountInRubles, string currencyCode)
+        {
+            var value = amountInRubles * exchangeRates.GetExchange(currencyCode);
+            return value >= 0 ? value : throw new UserFriendlyException("Отрицательное значение", value);
+        }
+    }
+}
